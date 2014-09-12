176f796115fd42cb1f10b5a9837bf3d63b85716a
DanElliottPalmer <danelliottpalmer@gmail.com>
2014-09-12 11:39:03 +0100

    Update: function comment

--- dist/bubble-point.js
+++ dist/bubble-point.js
@@ -3,6 +3,13 @@ function( Snap ){
 
   return Snap.plugin(function( Snap, Element, Paper ){
 
+    /**
+     * Draws a circle with a given x, y and area
+     * @param  {Number} x    
+     * @param  {Number} y    
+     * @param  {Number} area 
+     * @return {Snap.Circle}      
+     */
     Paper.prototype.bubblePoint = function( x, y, area ){
 
       var radius = Math.sqrt( area / Math.PI );
